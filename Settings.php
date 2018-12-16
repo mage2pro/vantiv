85bc1d2ee999ad8b4b7c125a9e4cd498516369cc
Dmitry Fedyuk <dfediuk@gmail.com>
2018-12-17 05:51:20 +0600

    no message

--- Settings.php
+++ Settings.php
@@ -2,34 +2,10 @@
 namespace Dfe\Vantiv;
 // 2018-12-17
 /** @method static Settings s() */
-final class Settings extends \Df\Payment\Settings\BankCard {
+final class Settings extends \Df\StripeClone\Settings {
 	/**
 	 * 2018-12-17
 	 * @return string
 	 */
-	function password() {return $this->p();}
-
-	/**
-	 * 2018-12-17
-	 * @override
-	 * @see \Df\Payment\Settings::publicKey()
-	 * @used-by \Df\StripeClone\ConfigProvider::config()
-	 * @return string
-	 */
-	function publicKey() {return null;}
-
-	/**
-	 * 2018-12-17
-	 * @return string
-	 */
-	function username() {return $this->v();}
-
-	/**
-	 * 2018-12-17
-	 * @override
-	 * @see \Df\Config\Settings::prefix()
-	 * @used-by \Df\Config\Settings::v()
-	 * @return string
-	 */
-	protected function prefix() {return 'df_payment/vantiv';}
+	function environment() {return $this->v();}
 }
