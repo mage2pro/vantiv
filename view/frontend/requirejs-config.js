5a2211c200fb0a54c1cb94a4adfb3754aa5dffb1
Dmitry Fedyuk <dfediuk@gmail.com>
2019-02-22 00:33:23 +0700

    1.1.1: https://github.com/mage2pro/vantiv/issues/3

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,4 +1,7 @@
-// 2019-02-21
+// 2019-02-22
 var config = {config: {mixins: {
+	// 2019-02-22
+	// «Make the Vantiv payment module compatible with a custom checkout module»
+	// https://github.com/mage2pro/vantiv/issues/3
 	'Magento_Checkout/js/view/payment/list': {'Dfe_Vantiv/payment-list': true}
 }}};
--- view/frontend/web/payment-list.js
+++ view/frontend/web/payment-list.js
@@ -1,2 +1,4 @@
-// 2019-02-21
+// 2019-02-22
+// «Make the Vantiv payment module compatible with a custom checkout module»
+// https://github.com/mage2pro/vantiv/issues/3
 define(['Dfe_Vantiv/loader'], function() {return function(sb) {return sb;};});
