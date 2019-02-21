a4a89710108ae7c0aa4fe39243f73586f70e0847
Dmitry Fedyuk <dfediuk@gmail.com>
2019-02-21 23:48:21 +0700

    1.1.0: https://github.com/mage2pro/vantiv/issues/3

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,4 @@
+// 2019-02-21
+var config = {config: {mixins: {
+	'Magento_Checkout/js/view/payment/list': {'Dfe_Vantiv/payment-list': true}
+}}};
---
+++ view/frontend/web/payment-list.js
@@ -0,0 +1,2 @@
+// 2019-02-21
+define(['Dfe_Vantiv/loader'], function() {return function(sb) {return sb;};});
