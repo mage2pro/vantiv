e48bccd32a1c2c292f447f6e16d3ee761539e1dc
Dmitry Fedyuk <dfediuk@gmail.com>
2019-04-04 06:55:21 +0600

    no message

--- API/Validator.php
+++ API/Validator.php
@@ -17,7 +17,7 @@ final class Validator extends \Df\API\Response\Validator {
 	 * 2018-12-19
 	 * @override
 	 * @see \Df\API\Response\Validator::valid()
-	 * @used-by \Df\API\Response\Validator::validate()
+	 * @used-by \Df\API\Client::_p()
 	 * @return bool
 	 */
 	function valid() {return '000' === $this->r('response');}
